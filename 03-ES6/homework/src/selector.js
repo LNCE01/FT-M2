6a298c24c8f5e702c4f478e60f8af0a716f74c80
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2023-07-15 14:35:59 +1000

    Finished else if for class

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -11,12 +11,10 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
   // TU CÓDIGO AQUÍ
 };
 
-// Detecta y devuelve el tipo de selector
-// devuelve uno de estos tipos: id, class, tag.class, tag
-
+// Detecta la posicion cero del string para determinar el selector y lo devuelve: id, class, tag.class, tag
 let selectorTypeMatcher = function (selector) {
   // tu código aquí
-  //selector = 'div' > tag // tag . class
+  //selector = 'div' > tag // tag .  class
   // #unID > id
   if (selector[0] === "#") {
     return "id";
@@ -32,15 +30,26 @@ let selectorTypeMatcher = function (selector) {
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
-// recuerda, la función matchFunction devuelta toma un elemento como un
-// parametro y devuelve true/false dependiendo si el elemento
-// matchea el selector.
+// recuerda, la función matchFunction devuelta toma un elemento como un parametro y devuelve true/false dependiendo si el elemento matchea el selector.
 
+//Si me pasan #myId
 var matchFunctionMaker = function (selector) {
-  var selectorType = selectorTypeMatcher(selector);
+  var selectorType = selectorTypeMatcher(selector); //el selector va a decir "sos un id"
   var matchFunction;
   if (selectorType === "id") {
+    matchFunction = function (el) {
+      //Ejemplo: por paramentro ingresa el tag return de la function selectorTypeMatcher, el element:
+      //<div id=myId></div> su element.id seria: myId y hay que compararlo con el selector #myId, por ende le concatenamos un hashtag al id del elemento.
+      return "#" + el.id === selector;
+    };
   } else if (selectorType === "class") {
+    matchFunction = function (el) {
+      var classes = el.classList; //clasess = ["clase1","clase2","clase3"]ß
+      for (let i = 0; i > classes.length; i++) {
+        if ("." + classes[i] === selector) return true; //Supongamos que pasan el selector ".clase3", el loop empieza a recorrer el array classList y cuando matchea, devuelve true
+      }
+      return false;
+    };
   } else if (selectorType === "tag.class") {
   } else if (selectorType === "tag") {
   }
