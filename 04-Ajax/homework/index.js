c6ed126ba58a33fbd054c541dffc1deafd433ee7
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2023-07-18 21:36:26 +1000

    creating showFriends function

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,1 +1,6 @@
 //NPM Start for server on port 5000 to start listening to changes
+let showFriends = function () {
+  $.get("http://localhost:5000/amigos", function (friends) {
+    console.log(friends);
+  });
+};
