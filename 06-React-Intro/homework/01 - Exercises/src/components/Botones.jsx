5f0fe12334401623f1ce681791cb5ad251e90d7f
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2023-07-25 14:18:07 +1000

    Added an onClick attribute to the buttons with an alert message

--- 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Botones = ()=>{
     return (
         <div>
-            
+            <button onClick={() => alert('Tu mensaje')}>Módulo 1</button>
+            <button onClick={() => alert('Tu mensaje')}>Módulo 2</button>
         </div>
     )
 }
