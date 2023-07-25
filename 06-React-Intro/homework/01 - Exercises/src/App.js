50d089e6717cd108fffa2cd3c35ca8d2eee69533
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2023-07-25 14:13:09 +1000

    Exercise 2_Imported React-Created function Botones-Returns a div tag

--- 06-React-Intro/homework/01 - Exercises/src/App.js
+++ 06-React-Intro/homework/01 - Exercises/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Bienvenido from './components/Bienvenido.jsx';
+import React from "react";
+import Bienvenido from "./components/Bienvenido.jsx";
+import Botones from "./components/Botones.jsx";
 
 export default function App() {
-   return (
-      <div>
-         <Bienvenido />
-      </div>
-   );
+  return (
+    <div>
+      <Bienvenido />
+    </div>
+  );
 }
